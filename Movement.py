2437beae581d64ae50833213c638c1f2a807b643
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-09-22 21:17:14 +0300

    movement

---
+++ Movement.py
@@ -0,0 +1,19 @@
+from serial.tools import list_ports
+import serial
+import time
+
+# List of existing ports. Chooses the right device if there are no others
+port = (str(list_ports.comports()[0]).split(' '))[0]
+ser=serial.Serial(port, 115200, timeout=0.00001)
+
+def forward(ser=ser):
+    ser.write(b'sd:0:-10:10\n')
+
+def right(ser=ser):
+    ser.write(b'sd:10:0:0\n')
+
+def left(ser=ser):
+    ser.write(b'sd:-10:0:0\n')
+
+def close(ser=ser):
+    ser.close()
---
+++ PlayBall.py
@@ -0,0 +1,5 @@
+from camera import *
+from Movement import *
+import time
+
+start =
