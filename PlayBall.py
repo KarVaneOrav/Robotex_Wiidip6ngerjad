3719bbc4d48063716651c2de2d30261a58ec118e
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-09-23 16:50:54 +0300

    'fix'

--- PlayBall.py
+++ PlayBall.py
@@ -24,6 +24,8 @@ try:
     while True:
         frame = Camera.processed_frame_green()
         cv2.imshow('RealSense', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         print("show frame")
         
         if jobs.get("look"): 
