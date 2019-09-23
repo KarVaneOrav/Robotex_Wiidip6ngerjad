9b781f6b2819996a200a00793a20cf3c3b6346c1
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-09-23 17:49:12 +0300

    'f'

--- Movement.py
+++ Movement.py
@@ -2,20 +2,20 @@ from serial.tools import list_ports
 import serial
 import time
 
-speed = '10'
+#speed = '10'
 
 # List of existing ports. Chooses the right device if there are no others
 port = (str(list_ports.comports()[0]).split(' '))[0]
 ser=serial.Serial(port, 115200, timeout=0.00001)
 
 def forward(ser=ser):
-    ser.write(b'sd:0:-'+speed+':'+speed+'\n')
+    ser.write(b'sd:0:-5:5\n')
 
 def right(ser=ser):
-    ser.write(b'sd:'+speed+':'+speed+':'+speed+'\n')
+    ser.write(b'sd:5:5:5\n')
 
 def left(ser=ser):
-    ser.write(b'sd:-'+speed+':-'+speed+':-'+speed+'\n')
+    ser.write(b'sd:-5:-5:-5\n')
 
 def close(ser=ser):
     ser.close()
