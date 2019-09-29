40ee2b08d34791458489595a56f9f95a01f1173d
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-09-29 20:11:32 +0300

    'f'

--- Camera.py
+++ Camera.py
@@ -81,9 +81,9 @@ def getDetector():
     return detector
 
 def ball_to_middle(balls):
-    if balls[0][0] < 300:
+    if balls[0][0] < 305:
         return [0, 0, -1]
-    elif balls[0][0] >340:
+    elif balls[0][0] >335:
         return [0, 0, 1]
     else:
         return [0, 0, 0]
