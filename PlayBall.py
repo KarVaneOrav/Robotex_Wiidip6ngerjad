2cf39b9cc4b9e8c5766053ea6ed90b2ed1265761
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-09-30 17:18:16 +0300

    'f'

--- PlayBall.py
+++ PlayBall.py
@@ -33,7 +33,7 @@ try:
                     print("looking")
             else:
                 turn = Camera.ball_to_middle(balls)
-                if turn == [0, 0, 0]:
+                #if turn == [0, 0, 0]:
                     #tasks["look"] = False
                 action(turn)
 ##        else:
