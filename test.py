9d1b1879736582dff70af1679ed59a527e33c038
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-10-02 00:32:53 +0300

    f

--- test.py
+++ test.py
@@ -1,12 +1,29 @@
-import Movement
-import time
+import cv2 as cv
+import numpy as np
+import Camera
 
-try:
-    while True:
-        Movement.thrower()
-        Movement.readSerial()
-        time.sleep(0.2)
-        
-finally:
-    print("done")
-    Movement.close()
+while True:
+    src = Camera.get_frame()
+
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+    gray = cv.medianBlur(gray, 5)
+
+    rows = gray.shape[0]
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                              param1=100, param2=30,
+                              minRadius=1, maxRadius=30)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv.circle(src, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv.circle(src, center, radius, (255, 0, 255), 3)
+
+    cv.imshow("detected circles", src)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
