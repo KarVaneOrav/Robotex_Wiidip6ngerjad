d8e6fc42594c92220cf6e8cc40a8edb65c374f2b
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-10-17 16:36:19 +0300

    f

--- Controller.py
+++ Controller.py
@@ -18,6 +18,7 @@ def timer():
         return False
 
 # w=119, a=97, s=115, d=100, o=111, p=112, q=113
+# angle 0 = right 90 = forward
 while True:
     cv2.namedWindow('rgb_img', cv2.WINDOW_NORMAL)
     key = cv2.waitKey(1)
@@ -25,4 +26,16 @@ while True:
         print(key)
     if timer():
         if key == 119:
-            Movement.motors(0.5, 0, 0)
+            Movement.motors(0.3, 90, 0)
+        elif key == 97:
+            Movement.motors(0.3, 180, 0)
+        elif key == 115:
+            Movement.motors(0.3, 270, 0)
+        elif key == 100:
+            Movement.motors(0.3, 0, 0)
+        elif key == 111:
+            Movement.motors(0, 0, 2)
+        elif key == 112:
+            Movement.motors(0, 0, -2)
+        else:
+            Movement.motors(0, 0, 0)
