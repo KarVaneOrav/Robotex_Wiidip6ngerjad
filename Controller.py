545669f72bf770e5a83d02ac985429e9a2348b30
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-10-17 16:47:04 +0300

    f

--- Controller.py
+++ Controller.py
@@ -17,25 +17,30 @@ def timer():
     else:
         return False
 
-# w=119, a=97, s=115, d=100, o=111, p=112, q=113
-# angle 0 = right 90 = forward
+
+# angle 0 = right 90 = forward (convert to rad)
 while True:
     cv2.namedWindow('rgb_img', cv2.WINDOW_NORMAL)
     key = cv2.waitKey(0)
     if key != -1:
         print(key)
     if timer():
-        if key == 119:
-            Movement.motors(0.3, 90, 0)
-        elif key == 97:
-            Movement.motors(0.3, 180, 0)
-        elif key == 115:
-            Movement.motors(0.3, 270, 0)
-        elif key == 100:
+        if key == 113:
+            break
+        elif key == 119:  # w
+            Movement.motors(0.3, 1.57, 0)
+        elif key == 97:  # a
+            Movement.motors(0.3, 3.14, 0)
+        elif key == 115:  # s
+            Movement.motors(0.3, 4.71, 0)
+        elif key == 100:  # d
             Movement.motors(0.3, 0, 0)
-        elif key == 111:
+        elif key == 111:  # o
             Movement.motors(0, 0, 2)
-        elif key == 112:
+        elif key == 112:  # p
             Movement.motors(0, 0, -2)
         else:
             Movement.motors(0, 0, 0)
+
+cv2.destroyAllWindows()
+Movement.close()
