b019cae3456dec2dc3db96851dfd61c722ea781f
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-10-25 13:36:59 +0300

    f

--- PlayBall2.py
+++ PlayBall2.py
@@ -4,7 +4,10 @@ import time
 import cv2
 import numpy as np
 
-green = [16, 163, 93, 124, 255, 255]  # threshold values
+green = [16, 163, 93, 124, 255, 255]  # threshold values, morph values
+pink = [95, 203, 61, 255, 255, 255, 3]
+blue = [35, 0, 29, 255, 91, 255, 3]
+opponent = 'blue'  # 'blue' or 'pink'
 
 
 def timer(pause):
@@ -19,25 +22,53 @@ def timer(pause):
 
 greenValues = {'lowerLimits': np.array([green[0], green[1], green[2]]),
                'upperLimits': np.array([green[3], green[4], green[5]])}
+targetValues = {'lowerLimits': None, 'upperLimits': None, 'kernelDilate': None}
+
+tasks = {"controller": False, "look": True, "rotate":  False, 'throw': False}
+frequency = 0.0166667  # send movement signals at 60Hz
+thrower_frequency = 0.002
+ball = []
+basket = []
 
 try:
     print("throwing1")
     Movement.thrower(1100)  # init thrower motor
 
     while True:
+        ball = []
+        basket = []
+
         frame = Camera.get_frame()  # to show vanilla frame
         hsv_frame = Camera.to_hsv(frame)
         processed_frame_green = Camera.process_balls(hsv_frame, greenValues)
 
         ball = Camera.green_finder(processed_frame_green)
-        if ball:
-            cv2.putText(frame, str(ball), tuple(ball),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-        cv2.imshow('RealSense', frame)
         key = cv2.waitKey(1)
         if key == 113:
             break
+
+        if tasks['look']:
+            print("looking")
+            if ball:
+                if ball[1] < 400:  # if ball is too far
+                    if timer(frequency):
+                        Movement.move_to_ball(ball)
+            else:
+                if timer(frequency):
+                    Movement.omni_drive([0, 0, 1])  # turns on the spot
+        else:
+            print("Error in tasks logic")
+            break
+
+        if ball:
+            cv2.putText(frame, str(ball), tuple(ball),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if basket:
+            cv2.putText(frame, str(basket), tuple(basket),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        cv2.imshow('RealSense', frame)
 finally:
     Camera.stop()
     Movement.close()
