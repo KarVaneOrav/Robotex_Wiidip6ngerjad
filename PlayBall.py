9a20e303996988d8c3b837ae3111ed0794a374dc
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-10-30 13:01:05 +0200

    f

--- PlayBall.py
+++ PlayBall.py
@@ -7,7 +7,7 @@ import numpy as np
 green = [16, 163, 93, 124, 255, 255]  # threshold values, morph values
 pink = [95, 203, 61, 255, 255, 255, 3]
 blue = [35, 0, 29, 255, 91, 255, 3]
-opponent = 'blue'  # 'blue' or 'pink'
+opponent = 'pink'  # 'blue' or 'pink'
 
 
 def timer(pause):
@@ -139,6 +139,9 @@ try:
 
         elif tasks['throw']:
             print("throwing")
+            basket = Camera.basket_finder(hsv_frame, targetValues)
+            distance = round(depth_frame.get_distance(basket[0], basket[1]), 1)
+            print("Distance:", distance)
             if throwing_cycle < 10:  # thrower speedup phase
                 if timer(thrower_frequency):
                     Movement.thrower(1900)
