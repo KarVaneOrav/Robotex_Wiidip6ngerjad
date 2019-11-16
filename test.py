8ce04a578f977851f5ef36fea99c4cdae57c0f6c
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-11-16 16:12:30 +0200

    f

--- test.py
+++ test.py
@@ -12,14 +12,15 @@ def ss(son):
 
 port = (str(list_ports.comports()[0]).split(' '))[0]
 ser = serial.Serial(port, 9600, timeout=0.00001)
-
+i = 0
 a = ''
 s = "START"
 p = "PING"
 st = "STOP"
-tekst = 'ssdsd'
+tekst = i
 
 while True:
+    i += 1
     while ser.inWaiting():
         tekst += ser.read()
 
