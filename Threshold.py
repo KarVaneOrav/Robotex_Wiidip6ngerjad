2cb02b3cc32af94ec6a250d6249c492279c4af91
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-12-01 08:58:49 +0200

    f

--- Threshold.py
+++ Threshold.py
@@ -11,7 +11,7 @@ def updateValue(bar, new_value):
 
 
 # trackbars
-bars = [43, 0, 0, 163, 92, 225, 3]
+bars = [13, 56, 36, 56, 213, 197]
 values = {'lowerLimits': None, 'upperLimits': None, 'kernelDilate': None}
 
 cv2.namedWindow('Controls', cv2.WINDOW_AUTOSIZE)
