43b8ea52455afc9ea4c352ac2f9cac33faeb427b
Taavet Jukola <taavet.joonas.jukola@tudeng.ut.ee>
2019-12-01 07:32:42 +0200

    f

--- distance_measurer2.py
+++ distance_measurer2.py
@@ -4,7 +4,7 @@ import cv2
 
 pink = [61, 128, 247, 255, 255, 255, 3]
 blue = [43, 0, 0, 163, 92, 147, 3]
-opponent = 'blue'  # 'blue' or 'pink'
+opponent = 'pink'  # 'blue' or 'pink'
 
 
 def set_target_basket(op):
